4c8dfd3bc2a1db9f9f5905b15459fe36db5f19d9
student <hemanthvenkat163@gmail.com>
2019-04-04 19:04:42 +0530

    gfh

---
+++ class.py
@@ -0,0 +1,19 @@
+
+
+class Calculator:
+
+	def add(a,b):
+		return a + b
+	def sub(a,b):
+		return a - b
+	def mul(a,b):
+		return a * b
+	def div(a,b):
+		return a / b
+c = Calculator
+print(c.add(10,3))
+print(c.sub(23,8))
+print(c.mul(4,6))
+print(c.div(6,3))
+
+
---
+++ fun1.py
@@ -0,0 +1,6 @@
+def add1(a,b=1):
+	return a + b
+x = add1(5,3)
+y =  add1(5)
+print(x)
+print(y)
