a483a53f3f5db2a3e51cfb24f9a600e08c7282d5
student <hemanthvenkat163@gmail.com>
2019-04-04 18:22:09 +0530

    hjfh

---
+++ d2.py
@@ -0,0 +1,10 @@
+x =int(input())
+rem = x%3
+if rem == 0:
+	print("divisible by 3")
+elif rem == 1:
+	print(rem)
+else:
+	print("not divisible by 3")
+
+	
---
+++ while1.py
@@ -0,0 +1,8 @@
+x = int(input("enter a number\n"))
+y = int(input("enter another number\n"))
+i = 0
+while(i<=10):
+	print(x)
+	print(y)
+	i+=1
+
